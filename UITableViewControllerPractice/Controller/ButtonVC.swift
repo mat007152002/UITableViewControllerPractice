8d374e5a6e4ad24db65ff92e4240e1d00654e3d7
Matthew Ling <mat007152002g@gmail.com>
2020-05-13 14:57:02 +0800

    add page success

---
+++ UITableViewControllerPractice/Controller/ButtonVC.swift
@@ -0,0 +1,30 @@
+//
+//  ButtonVC.swift
+//  UITableViewControllerPractice
+//
+//  Created by 旌榮 凌 on 2020/5/13.
+//  Copyright © 2020 旌榮 凌. All rights reserved.
+//
+
+import UIKit
+
+class ButtonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
